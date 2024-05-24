0f36db7a8266bfa08687fa32d035cbf78bbd9914
Loquias13 <ritsuloquias@gmail.com>
2024-05-25 00:01:51 +0800

    Test

--- docs.php
+++ docs.php
@@ -314,7 +314,7 @@
 		    <div class="container-xl">
 			    <div class="row g-3 mb-4 align-items-center justify-content-between">
 				    <div class="col-auto">
-			            <h1 class="app-page-title mb-0">My Docs</h1>
+			            <h1 class="app-page-title mb-0">Maps</h1>
 				    </div>
 				    <div class="col-auto">
 					     <div class="page-utilities">
